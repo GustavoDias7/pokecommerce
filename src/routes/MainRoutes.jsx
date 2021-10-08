e03ab44971629dacb1109c06e741af5203dfefce
Gustavo <gustavo7dias@gmail.com>
2021-10-08 12:54:46 -0300

    Add header

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -4,19 +4,22 @@ import Home from "../pages/Home";
 import Checkout from "../pages/Checkout";
 import NotFound from "../pages/NotFound";
 import PokemonDetails from "../pages/PokemonDetails";
+import { GlobalStorage } from "../context/GlobalContenxt";
+import MainHeader from "../components/MainHeader";
 
 const MainRoutes = () => {
   return (
-    <>
-      <BrowserRouter>
+    <BrowserRouter>
+      <GlobalStorage>
+        <MainHeader />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="checkout" element={<Checkout />} />
           <Route path="pokemon/:id" element={<PokemonDetails />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
-    </>
+      </GlobalStorage>
+    </BrowserRouter>
   );
 };
 
